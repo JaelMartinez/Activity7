3d2755472fb41283b3cdebcde89e685682fe9539
JaelMartinez <lacarei1221@gmail.com>
2024-03-15 18:25:42 -0600

    tablas

---
+++ database/migrations/2024_03_15_233723_create_courses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('cover_image');
+            $table->text('content');
+            $table->unsignedBigInteger('robotics_kit_id');
+            $table->foreign('robotics_kit_id')->references('id')->on('robotics_kits')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+}
